3831228e6cf1dd2cb8cf844a64e4f29f41ca5717
shivam <2k25csai2513893@gmail.com>
2026-09-21 23:13:34 +0530

    Time: 0 ms (100%), Space: 8.3 MB (90.1%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
     string reversePrefix(string s, char ch) {
-        string ans="";
-        int l=s.size();
-        stack<char>st;
+        int l=s.length();
+       
         int i=0;
         bool flag=0;
         while(i<l){
             if(s[i]==ch){
+                
                 flag=1;
-                st.push(ch);
-                i++;
+               
                 break;
             }
-            st.push(s[i]);
+           
             i++;
+            
         }
         if(flag==0){
             return s;
         }
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop();
+        int j=0;
+        while(j<=i){
+            swap(s[j],s[i]);
+            j++;
+            i--;
         }
-       while(i<l){
-         ans.push_back(s[i]);
-          i++;
-       }
-        return ans;
-        
+        return s;
     }
 };
